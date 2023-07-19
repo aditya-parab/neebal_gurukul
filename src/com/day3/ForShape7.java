89abb22dcfe9e6e611e24173686c351496c1b44d
aditya <adparab97+0@gmail.com>
2023-07-18 23:00:35 -0700

    For loop shapes MORE

---
+++ src/com/day3/ForShape7.java
@@ -0,0 +1,16 @@
+package com.day3;
+
+public class ForShape7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=4;i++) {
+			for(int j=i;j>=0;j--) {
+				System.out.print(5-j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
