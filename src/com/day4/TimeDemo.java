a13b4ea6808ce313d45f78b0cee8dfd2994599fc
aditya <adparab97+0@gmail.com>
2023-07-20 08:39:56 -0700

    initial TimeDemo implementation 1

--- src/com/day4/TimeDemo.java
+++ src/com/day4/TimeDemo.java
@@ -1,30 +1,30 @@
 package com.day4;
 
-public class Time {
+public class TimeDemo {
 private int hours,min,sec;
-	public Time() {
+	public TimeDemo() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	Time(int hours, int min, int sec){
+	TimeDemo(int hours, int min, int sec){
 		this.hours = hours;
 		this.min = min;
 		this.sec = sec;
 	}
 	
-	public Time add(Time t) {
+	public TimeDemo add(TimeDemo t) {
 		int sec1 = (this.hours%12)*(60*60)+this.min*(60)+this.sec;
 		int sec2 = (t.hours%12)*(60*60)+t.min*(60)+t.sec;
 		int totalsecs = sec1+sec2;
 		int hr3 = totalsecs/(3600);
 		int min3 = (totalsecs%(60*60))/60;
 		int sec3 = (totalsecs%(60*60))%(60);
-		return new Time(hr3,min3,sec3);
+		return new TimeDemo(hr3,min3,sec3);
 	}
 	
 	public String convertIn12Hours() {
 		
-		Time temp = new Time(this.hours,this.min,this.sec);
+		TimeDemo temp = new TimeDemo(this.hours,this.min,this.sec);
 		String timetype="";
 		if(temp.hours>=12) {
 			timetype = "PM";
@@ -46,9 +46,9 @@ private int hours,min,sec;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Time x = new Time(12,30,25);
-		Time y = new Time(19,20,35);
-		Time z = x.add(y);
+		TimeDemo x = new TimeDemo(12,30,25);
+		TimeDemo y = new TimeDemo(19,20,35);
+		TimeDemo z = x.add(y);
 		
 
 		
